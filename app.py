5ee00f7ae42d95ee459e7809f51994b31476c113
hkimskh <hkimskh@gmail.com>
2025-09-02 21:24:10 +0700

    materi python

--- app.py
+++ app.py
@@ -2,3 +2,7 @@ print("BELAJAR GIT DASAR")
 # git add [nama_file_yang_perubahan]
 # git commit -m "isi_pesan_perubahan"
 # git push -u origin main
+#---- cara tambah sekali banyak file-----
+#git add.
+#git commit -m "isi pesan"
+#git push -u origin main
