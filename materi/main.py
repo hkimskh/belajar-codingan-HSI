c52af6cf51439471779cb29c8ad3276048d69b5d
hkimskh <hkimskh@gmail.com>
2025-09-05 20:14:25 +0700

    materi python dasar

--- materi/main.py
+++ materi/main.py
@@ -1,1 +1,23 @@
-print("Hello World")
+import csv
+
+file_path = r"/home/abdulhakim/Dokumen/dev/murajaah-csv/data.csv"
+
+with open(file_path, "r") as file_baru:
+    next(file_baru)
+    read = csv.reader(file_baru)
+    list_read =list(read)
+    #pake list biar g muncul objek objek objek objek 
+    print("semua data")
+    print("-" * 25)
+    for baris in list_read:
+        nomor = baris[0]
+        nama = baris[1]
+        kelas = baris[2]
+
+        print(f"| {nomor} | {nama} | {kelas} |")
+
+
+    with open(file_path, 'a', newline="") as file_baru:
+         write = csv.writer(file_baru)
+         write.writerow({"5","Bilal","10"})
+    
