df39f1679da6db9aede01235d82fbd6970ba325f
hkimskh <hkimskh@gmail.com>
2026-02-04 21:29:51 +0700

    python

--- Semester 2/PYTHON OOP/tugas python sem 2/crud.py
+++ Semester 2/PYTHON OOP/tugas python sem 2/crud.py
@@ -1,31 +1,86 @@
+# 
+
+
 data = []
-# data sebagai array
+
+def cari_data(id):
+    for d in data:
+        if d["id"] == id:
+            return d
+    return None
+
 
 def tambah_data():
-    # variable = .....
-    id = int(input("Masukkan ID: "))
-    nama = input("Masukkan Nama: ")
-    kelas = input("Masukkan Kelas: ")
+    print("\nTambah Data")
+    id = int(input("ID: "))
+    if cari_data(id):
+        print("ID sudah ada")
+        return
 
-
-    # append fungsinya adalah untuk menambahkan data ke dalam array
     data.append({
         "id": id,
-        "nama": nama,
-        "kelas": kelas
+        "nama": input("Nama: "),
+        "kelas": input("Kelas: ")
     })
+    print("Data ditambah")
 
-    print("Data berhasil ditambahkan!")
 
-
-def tampilkan_data():
-    # len = panjang data
-    if len(data) == 0:
-        print("Data kosong.")
+def tampil_data():
+    print("\nData Siswa")
+    if not data:
+        print("Data kosong")
         return
-    
-    for item in data:
-        print("ID:", item["id"], "Nama:", item["nama"], "Dari kelas:", item["kelas"])
 
-tambah_data()
-tampilkan_data()
+    print("ID | Nama | Kelas")
+    for d in data:
+        print(d["id"], "|", d["nama"], "|", d["kelas"])
+
+
+def ubah_data():
+    print("\nUbah Data")
+    id = int(input("ID: "))
+    d = cari_data(id)
+
+    if not d:
+        print("Data tidak ditemukan")
+        return
+
+    d["nama"] = input("Nama baru: ")
+    d["kelas"] = input("Kelas baru: ")
+    print("Data diubah")
+
+
+def hapus_data():
+    print("\nHapus Data")
+    id = int(input("ID: "))
+    d = cari_data(id)
+
+    if not d:
+        print("Data tidak ditemukan")
+        return
+
+    data.remove(d)
+    print("Data dihapus")
+
+
+while True:
+    print("\n1. Tambah")
+    print("2. Tampil")
+    print("3. Ubah")
+    print("4. Hapus")
+    print("0. Keluar")
+
+    pilih = input("Pilih: ")
+
+    if pilih == "1":
+        tambah_data()
+    elif pilih == "2":
+        tampil_data()
+    elif pilih == "3":
+        ubah_data()
+    elif pilih == "4":
+        hapus_data()
+    elif pilih == "0":
+        break
+    else:
+        print("Menu tidak ada")
